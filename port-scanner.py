eb993d51259be2ffb77f0740382b6b4eef548c9f
Eddie Oghomwen <eddyoghomwen@gmail.com>
2024-07-18 00:11:12 -0400

    Fix in syntax.

--- port-scanner.py
+++ port-scanner.py
@@ -11,9 +11,9 @@ remoteServer = input("Enter a remote host to scan: ")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
 #Print a nice banner with information on which host we are about to scan
-print ("_") * 60
+print ("_ * 60")
 print ("Please wait. scanning remote host", remoteServerIP)
-print (" ") * 60
+print (" * 60")
 
 #Check the date and time the scan was started
 t1 = datetime.now()
@@ -22,7 +22,7 @@ t1 = datetime.now()
 #Also we will do error handling 
 
 try:
-    for port in range (1.5000):
+    for port in range (1,5000):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
