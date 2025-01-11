ea6d9814e2b0f8f80efacde32f2a1a1a966b8910
Edmund Oghomwen <105716192+ogedmund@users.noreply.github.com>
2025-01-11 10:07:13 -0500

    Update webscraperHP.py
    Added comments and a file function to save results

--- webscraperHP.py
+++ webscraperHP.py
@@ -5,11 +5,16 @@ URL = "https://www.projecthoneypot.org/list_of_ips.php"
 html_content = requests.get(URL)
 soup = BeautifulSoup(html_content, 'html.parser')
 
-ip_addresses = [] 
-for row in soup.select('table.manmx tr')[1:]:
+ip_addresses = [] #store the data being read
+for row in soup.select('table.manmx tr')[1:]: #read the data but skip first line ([1:0])
     ip_cell = row.select_one('td a.bnone') 
     if ip_cell:
         ip_addresses.append(ip_cell.text.strip())
 
 for ip in ip_addresses: 
     print(ip)
+
+with open('iplist.txt', 'w') as file:  #Assuming there was an iplist.txt file to save the result to
+    for ip in ip_addresses: 
+        print(ip)
+        file.write(ip + '\n')"""
