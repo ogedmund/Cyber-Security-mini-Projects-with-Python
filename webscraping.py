1a2b3abe7b97b931bddeb436ee40715fa6b7ff91
Edmund Oghomwen <105716192+ogedmund@users.noreply.github.com>
2025-01-11 09:53:44 -0500

    Update webscraping.py
    Added some comments

--- webscraping.py
+++ webscraping.py
@@ -1,8 +1,8 @@
-import requests from bs4 
-import BeautifulSoup 
-URL = "https://letsdefend.io" 
-page = requests.get(URL) 
-soup = BeautifulSoup(page.content, "html.parser") 
+import requests from bs4 #The requests module allows communicating with web servers using the HTTP protocol
+import BeautifulSoup #BeautifulSoup library is used to parse HMTL and XML docs
+URL = "https://letsdefend.io"  
+page = requests.get(URL) # HTTP GET request is sent to the specified URL using the "requests.get()" method
+soup = BeautifulSoup(page.content, "html.parser") #instance of the BeautifulSoup class
 results = soup.find("meta", {"name":"description"}) 
 
 print(results)
