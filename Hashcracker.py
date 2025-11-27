2bd30db96f7cbe1b000a14939f78b900a5d72521
Edmund Oghomwen <105716192+ogedmund@users.noreply.github.com>
2025-11-27 17:17:56 -0500

    Create Hashcracker.py

---
+++ Hashcracker.py
@@ -0,0 +1,26 @@
+import hashlib
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("TryHackMe \n Python 4 Pentesters \n HASH CRACKER for MD 5")
+print(ascii_banner)
+
+wordlist_location = str(input('Enter wordlist file location: '))
+hash_input = str(input('Enter hash to be cracked: '))
+
+with open(wordlist_location, 'r') as file:
+    for line in file.readlines():
+        hash_ob = hashlib.md5(line.strip().encode())
+        hashed_pass = hash_ob.hexdigest()
+        if hashed_pass == hash_input:
+            print('Found cleartext password! ' + line.strip())
+            exit(0)
+
+"""
+The script above follows the approach below;
+
+Asks for the location of a wordlist
+Asks for the hash to be cracked
+Reads values from the wordlist (one per line)
+Converts cleartext values to MD5 hash values
+Compares the generated MD5 hash value with the value entered by the user
+"""
